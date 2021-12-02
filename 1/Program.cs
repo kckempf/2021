d7a64ee15082859b35265e4a1f504cd3408f0ce7
Kevin Kempf <kckempf@gmail.com>
2021-12-02 10:54:33 -0500

    initial checkin

---
+++ 1/Program.cs
@@ -0,0 +1,21 @@
+﻿var depths = File.ReadAllLines(Path.GetFullPath("input.txt"));
+int.TryParse(depths[0], out int depth);
+var output = 0;
+for (int i = 1; i < depths.Length; i++)
+{
+    int.TryParse(depths[i], out int currentDepth);
+    if (currentDepth > depth)
+        output++;
+    depth = currentDepth;
+}
+Console.WriteLine($"Answer 1: {output}");
+
+output = 0;
+for (int i = 3; i < depths.Length; i++)
+{
+    int.TryParse(depths[i], out int currentDepth);
+    int.TryParse(depths[i - 3], out int pastDepth);
+    if (currentDepth > pastDepth)
+        output++;
+}
+Console.WriteLine($"Answer 2: {output}");
---
+++ 2/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+var instructions = File.ReadAllLines(Path.GetFullPath("input.txt"));
+var depth = 0;
+var x = 0;
+foreach (var step in instructions)
+{
+    var split = step.Split(' ');
+    var direction = split[0];
+    int.TryParse(split[1], out int amp);
+    switch (direction)
+    {
+        case "forward":
+            x += amp;
+            break;
+        case "down":
+            depth += amp;
+            break;
+        case "up":
+            depth -= amp;
+            break;
+        default:
+            break;
+    }
+}
+Console.WriteLine($"{x * depth}");
+depth = 0;
+x = 0;
+var aim = 0;
+foreach (var step in instructions)
+{
+    var split = step.Split(' ');
+    var direction = split[0];
+    int.TryParse(split[1], out int amp);
+    switch (direction)
+    {
+        case "forward":
+            x += amp;
+            depth += amp * aim;
+            break;
+        case "down":
+            aim += amp;
+            break;
+        case "up":
+            aim -= amp;
+            break;
+        default:
+            break;
+    }
+}
+Console.WriteLine($"{x * depth}");
