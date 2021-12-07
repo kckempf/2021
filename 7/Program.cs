1b52994386af31d3c272bc6e288052d763f6f412
Kevin Kempf <kckempf@gmail.com>
2021-12-07 11:50:30 -0500

    Day 7 Part 2

--- 7/Program.cs
+++ 7/Program.cs
@@ -10,3 +10,33 @@ for (int i = 1; i < instructions.Length; i++)
         break;
 }
 Console.WriteLine($"{result}");
+
+var first = instructions[0];
+var last = instructions[instructions.Length - 1];
+var midSum = 0;
+while (first < last)
+{
+    var mid = first + (last - first) / 2;
+    midSum = instructions.Sum(x=>InnerSum(x, mid));
+    var beforeSum = instructions.Sum(x=>InnerSum(x, mid - 1));
+    var afterSum = instructions.Sum(x=>InnerSum(x, mid + 1));
+    if (midSum < beforeSum && midSum < afterSum)
+    {
+        break;
+    }
+    else if (midSum > beforeSum)
+    {
+        last = mid - 1;
+    }
+    else if (midSum > afterSum)
+    {
+        first = mid;
+    }
+}
+
+Console.WriteLine($"{midSum}");
+int InnerSum(int a, int b)
+{
+    var n = Math.Abs(a - b);
+    return (n * (n + 1)) / 2;
+}
