528b1bfcd0db13fa5f2a5312b07f188ca6f9235e
Kevin Kempf <kckempf@gmail.com>
2021-12-06 20:23:34 -0500

    Day 6, both stars

---
+++ 6/Program.cs
@@ -0,0 +1,27 @@
+﻿var instructions = File.ReadAllLines(Path.GetFullPath("input.txt")); // read instructions from file
+var fileInput = instructions[0].Split(',').Select(x=>int.Parse(x));
+Dictionary<int, long> nums = new Dictionary<int, long>();
+long result = 0;
+foreach (var input in fileInput)
+    result += Recursion((6 - input) + 80);
+Console.WriteLine($"{result}");
+
+var example = new int[]{3, 4, 3, 1, 2};
+
+result = 0;
+foreach (var input in fileInput)
+    result += Recursion((6 - input) + 256);
+Console.WriteLine($"{result}");
+
+long Recursion(int input)
+{
+    long output = 1;
+    if (nums.ContainsKey(input))
+        return nums[input];
+    else if (input < 7)
+        output = 1;
+    else
+        output = Recursion(input - 7) + Recursion(input - 9);
+    nums[input] = output;
+    return output;
+}
