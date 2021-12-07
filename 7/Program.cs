5af9273a6e40507b842630fa583ab4144f025bc4
Kevin Kempf <kckempf@gmail.com>
2021-12-07 08:23:41 -0500

    Day 7 Part 1

---
+++ 7/Program.cs
@@ -0,0 +1,12 @@
+﻿var instructions = File.ReadAllLines(Path.GetFullPath("input.txt"))[0].Split(',').Select(x=>int.Parse(x)).ToArray();
+Array.Sort(instructions);
+int result = instructions.Sum(x => Math.Abs(instructions[0] - x));
+for (int i = 1; i < instructions.Length; i++)
+{
+    var sum = instructions.Sum(x => Math.Abs(instructions[i] - x));
+    if (sum <= result)
+        result = sum;
+    else
+        break;
+}
+Console.WriteLine($"{result}");
