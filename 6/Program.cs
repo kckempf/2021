c7bc963f623c4b8442719db57ba18cad61607dd2
Kevin Kempf <kckempf@gmail.com>
2021-12-08 10:23:30 -0500

    Adding comments for the folks

--- 6/Program.cs
+++ 6/Program.cs
@@ -1,27 +1,27 @@
-﻿var instructions = File.ReadAllLines(Path.GetFullPath("input.txt")); // read instructions from file
-var fileInput = instructions[0].Split(',').Select(x=>int.Parse(x));
-Dictionary<int, long> nums = new Dictionary<int, long>();
+﻿var instructions = File.ReadAllLines(Path.GetFullPath("input.txt"));    // read instructions from file
+var fileInput = instructions[0].Split(',').Select(x=>int.Parse(x));     // convert instructions to ints
+Dictionary<int, long> nums = new Dictionary<int, long>();               // memoization dictionary
 long result = 0;
 foreach (var input in fileInput)
-    result += Recursion((6 - input) + 80);
-Console.WriteLine($"{result}");
+    result += Recursion((6 - input) + 80);                              // call the recursive function for each
+Console.WriteLine($"{result}");                                         // each one takes 6 - n steps further than 80
 
 var example = new int[]{3, 4, 3, 1, 2};
 
 result = 0;
 foreach (var input in fileInput)
-    result += Recursion((6 - input) + 256);
+    result += Recursion((6 - input) + 256);                             // part two
 Console.WriteLine($"{result}");
 
 long Recursion(int input)
 {
-    long output = 1;
-    if (nums.ContainsKey(input))
-        return nums[input];
-    else if (input < 7)
+    long output = 1;                                                    // result is more than 2^32 - 1
+    if (nums.ContainsKey(input))                                        // use memoization to only calculate once
+        return nums[input];                     
+    else if (input < 7)                                                 // start with one lanternfish
         output = 1;
     else
-        output = Recursion(input - 7) + Recursion(input - 9);
+        output = Recursion(input - 7) + Recursion(input - 9);           // add another every 7 steps; it's 2 steps behind
     nums[input] = output;
     return output;
 }
