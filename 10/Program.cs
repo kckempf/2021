6aa55fdee96efcb76f0b823ab54ebbd95a804e91
Kevin Kempf <kckempf@gmail.com>
2021-12-10 08:15:01 -0500

    Day 10, Parts 1 and 2

---
+++ 10/Program.cs
@@ -0,0 +1,60 @@
+﻿var instructions = File.ReadAllLines(Path.GetFullPath("input.txt")).Select(x => x.ToCharArray()).ToArray();
+var illegalScores = new Dictionary<char, int>()
+{
+    { ')', 3},
+    { ']', 57},
+    { '}', 1197},
+    { '>', 25137}
+};
+var charScores = new Dictionary<char, int>()
+{
+    { ')', 1},
+    { ']', 2},
+    { '}', 3},
+    { '>', 4}
+};
+var legalEnds = new Dictionary<char, char>()
+{
+    { '(', ')'},
+    { '[', ']'},
+    { '{', '}'},
+    { '<', '>'}
+};
+var output = 0;
+var completionScores = new List<long>();
+foreach (var instruction in instructions)
+{
+    var stack = new Stack<char>();                      
+    long completionScore = 0;
+    var legal = true;
+    for (int i = 0; i < instruction.Length; i++)
+    {
+        var currentChar = instruction[i];
+        if (legalEnds.ContainsKey(currentChar))                 // Stack the legal ends when we see a start
+        {
+            stack.Push(legalEnds[currentChar]);
+        }
+        else
+        {
+            var expectedChar = stack.Pop();
+            if (currentChar != expectedChar)                    // If end is unexpected, corrupted
+            {
+                output += illegalScores[currentChar];
+                legal = false;
+                break;
+            }
+        }
+    }
+    while (stack.Count > 0 && legal)                            // stack has all the missing ends
+    {
+        var expectedChar = stack.Pop();
+        completionScore *= 5;
+        completionScore += charScores[expectedChar];
+    }
+    if (legal)
+        completionScores.Add(completionScore);
+}
+completionScores.Sort();
+var output2 = completionScores[(completionScores.Count / 2)];  
+Console.WriteLine($"{output}");
+Console.WriteLine($"{output2}");
