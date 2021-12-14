97ce6767d8e8af38e1140fe2055bde51259d7a0c
Kevin Kempf <kckempf@gmail.com>
2021-12-14 17:08:37 -0500

    Better approach for part 2?

--- 14/Program.cs
+++ 14/Program.cs
@@ -20,29 +20,60 @@ var sample = new string[]{
     "CN -> C"
 };
 
+var pairs = new Dictionary<string, ulong>();
+var chars = new ulong[26];
 var template = input[0];
-var mappings = new Dictionary<string, char>();
-
-for (int i = 2; i < input.Length; i++)
+for (int i = 0; i < template.Length - 1; i++)
+{
+    chars[template[i] - 'A'] += 1;
+    var pair = template.Substring(i, 2);
+    if (pairs.ContainsKey(pair))
+        pairs[pair] += 1;
+    else
+        pairs[pair] = 1;
+}
+chars[template[template.Length - 1] - 'A'] += 1;
+var mappings = new Dictionary<string, string[]>();
+for (int i = 2; i < input.Count(); i++)
 {
     var row = input[i].Split(" -> ");
-    mappings.Add(row[0], row[1][0]);
+    var first = row[0][0] + row[1];
+    var second = row[1] + row[0][1];
+    mappings.Add(row[0], new string[] { first, second });
 }
 
-for (int j = 0; j < 10; j++)
+for (int i = 0; i < 10; i++)
 {
-    var templateList = template.ToCharArray().ToList();
-    for (int i = template.Length - 1; i > 0; i--)
+    foreach (var pair in pairs)
     {
-        var segment = template.Substring(i - 1, 2);
-        if (mappings.ContainsKey(segment))
-        {
-            templateList.Insert(i, mappings[segment]);
-        }
+        var replace = mappings[pair.Key];
+        pairs[pair.Key] -= 1;
+        pairs[replace[0]] += 1;
+        pairs[replace[1]] += 1;
+        var newChar = replace[0][1];
+        chars[newChar - 'A'] += 1;
     }
-    template = new string(templateList.ToArray());
 }
 
-var m = template.ToCharArray().ToList().GroupBy(x => x).OrderByDescending(y=>y.Count()).Select(p=>p.Count()).FirstOrDefault();
-var n = template.ToCharArray().ToList().GroupBy(x => x).OrderBy(y=>y.Count()).Select(p=>p.Count()).FirstOrDefault();
-Console.WriteLine($"{m - n}");
+var min = chars.Min();
+var max = chars.Max();
+
+Console.WriteLine($"{max - min}");
+
+
+// "CH -> B",   CB, BH   b+
+// "HH -> N",   HN, NH
+// "CB -> H",   CH, BH
+// "NH -> C",   NC, CH
+// "HB -> C",   HC, CB
+// "HC -> B",   HB, BC
+// "HN -> C",   HC, CN
+// "NN -> C",   NC, CN
+// "BH -> H",   BH, HH
+// "NC -> B",   NB, BC
+// "NB -> B",   NB, BB
+// "BN -> B",   BB, BN
+// "BB -> N",   BN, NB
+// "BC -> B",   BB, BC
+// "CC -> N",   CN, NC
+// "CN -> C"    CC, CN
