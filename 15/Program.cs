e588732b143f3495970c34634c4faa9257a8333b
Kevin Kempf <kckempf@gmail.com>
2021-12-15 11:49:00 -0500

    Day 15 Part 2

--- 15/Program.cs
+++ 15/Program.cs
@@ -1,33 +1,52 @@
 ﻿var input = File.ReadAllLines(Path.GetFullPath("input.txt")).Select(x => x.ToCharArray().Select(x => x - '0').ToArray()).ToArray();
-var dirs = new int[][]{                         // Set up the directions
-    new int[]{-1, 0},
-    new int[]{0, -1},
-    new int[]{1, 0},
-    new int[]{0, 1}
-};
-var pq = new PriorityQueue<int[], int>();       // Initialize Priority Queue
-pq.Enqueue(new int[] { 0, 0 }, 0);
-var cell = new int[] { 0, 0 };
-var output = 0;
-var sum = 0;
-while (pq.Count > 0 && (cell[0] < input.Length - 1 || cell[1] < input[0].Length - 1))  // Dykstra's Shortest Path
+var width = input.Length;
+var output = ShortestPath(input, 5, width);
+
+Console.WriteLine($"{output}");
+
+int ShortestPath(int[][] input, int multiplier, int width)
 {
-    pq.TryDequeue(out cell, out sum);
-    Console.WriteLine($"{cell[0]},{cell[1]}, sum: {sum}");
-    foreach (var dir in dirs)
+    var dirs = new int[][]{                         // Set up the directions
+        new int[]{-1, 0},
+        new int[]{0, -1},
+        new int[]{1, 0},
+        new int[]{0, 1}
+    };
+    var visited = new HashSet<string>();            // Record whether we have been here before
+    var pq = new PriorityQueue<int[], int>();       // Initialize Priority Queue
+    pq.Enqueue(new int[] { 0, 0 }, 0);
+    var cell = new int[] { 0, 0 };
+    var output = 0;
+    var sum = 0;
+    while (
+        pq.Count > 0 &&
+        (cell[0] < input.Length * multiplier - 1 ||
+        cell[1] < input[0].Length * multiplier - 1))    // Dykstra's Shortest Path
     {
-        var nextCell = new int[] { cell[0] + dir[0], cell[1] + dir[1] };
-        if (
-            nextCell[0] > -1 &&
-            nextCell[0] < input.Length &&
-            nextCell[1] > -1 &&
-            nextCell[1] < input[0].Length &&
-            input[nextCell[0]][nextCell[1]] != -1)
+        pq.TryDequeue(out cell, out sum);               // Prioriy Queue keeps items in order by value, by ascending numeric by default
+        foreach (var dir in dirs)
         {
-            var density = sum + input[nextCell[0]][nextCell[1]];
-            input[nextCell[0]][nextCell[1]] = -1;
-            pq.Enqueue(nextCell, density);
+            var nextCell = new int[] { cell[0] + dir[0], cell[1] + dir[1] };
+            var i = nextCell[0] % width;
+            var j = nextCell[1] % width;
+            if (
+                nextCell[0] > -1 &&
+                nextCell[0] < input.Length * multiplier &&
+                nextCell[1] > -1 &&
+                nextCell[1] < input[0].Length * multiplier &&
+                !visited.Contains($"{nextCell[0]} - {nextCell[1]}"))    // Don't go where we have been
+            {
+                var addI = nextCell[0] / width;
+                var addJ = nextCell[1] / width;
+                var newSum = input[i][j];
+                newSum = newSum + addI + addJ;
+                newSum = newSum % 9;
+                newSum = newSum == 0 ? 9 : newSum;
+                var density = sum + newSum;
+                visited.Add($"{nextCell[0]} - {nextCell[1]}");  // Record when we have been here
+                pq.Enqueue(nextCell, density);                  // density = sum of density so far
+            }
         }
     }
+    return sum;
 }
-Console.WriteLine($"sum: {sum}, pq.Count: {pq.Count}");
