ac4fc8237fbc3bc11fee0efb7801b8518aeddec2
Kevin Kempf <kckempf@gmail.com>
2021-12-20 08:14:37 -0500

    Day 18 progress

--- 18/Program.cs
+++ 18/Program.cs
@@ -1,22 +1,36 @@
-﻿//var input = File.ReadAllLines(Path.GetFullPath("input.txt"));
-var input = new string[]
-{
-    "[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]",
-    "[[[5,[2,8]],4],[5,[[9,9],0]]]",
-    "[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]",
-    "[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]",
-    "[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]",
-    "[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]",
-    "[[[[5,4],[7,7]],8],[[8,3],8]]",
-    "[[9,3],[[9,9],[6,[4,9]]]]",
-    "[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]",
-    "[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]"
+﻿using System.Text.RegularExpressions;
+//var input = File.ReadAllLines(Path.GetFullPath("input.txt"));
+// var input = new string[]
+// {
+//     "[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]",
+//     "[[[5,[2,8]],4],[5,[[9,9],0]]]",
+//     "[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]",
+//     "[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]",
+//     "[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]",
+//     "[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]",
+//     "[[[[5,4],[7,7]],8],[[8,3],8]]",
+//     "[[9,3],[[9,9],[6,[4,9]]]]",
+//     "[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]",
+//     "[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]"
+// };
+var input = new string[]{
+"[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]",
+"[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]",
+"[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]",
+"[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]",
+"[7,[5,[[3,8],[1,4]]]]",
+"[[2,[2,2]],[8,[8,1]]]",
+"[2,9]",
+"[1,[[[9,3],9],[[9,0],[0,7]]]]",
+"[[[5,[7,4]],7],1]",
+"[[[[4,2],2],6],[8,7]]"
 };
 
 var blah = input[0];
 for (int i = 1; i < input.Count(); i++)
 {
     blah = Add(blah, input[i]);
+    blah = Reduce(blah);
     Console.WriteLine($"{i}: {blah}");
 }
 string Add(string a, string b)
@@ -62,28 +76,34 @@ string Reduce(string input)
 
 string Split(string input, int first)
 {
+    Console.WriteLine(input);
     var splitString = input.Substring(first, 2);
-    var splitInput = input.Split(splitString);
     var splitInt = int.Parse(splitString);
     var insertString = $"[{splitInt / 2},{splitInt / 2 + splitInt % 2}]";
-    return splitInput[0] + insertString + splitInput[1];
+    Regex ex = new Regex(splitString);
+
+    return ex.Replace(input, insertString, 1);
 }
 
 string Explode(string input, int previousNum, int fifthBracket)
 {
+    Console.WriteLine(input);
     int closeBracket = fifthBracket;
     while (input[closeBracket] != ']')
         closeBracket++;
     var pair = input.Substring(fifthBracket + 1, closeBracket - fifthBracket - 1).Split(',');
     int nextNum = closeBracket;
-    
-    int N = input.Length;
+
     while (nextNum < input.Length && !char.IsNumber(input[nextNum]))
         nextNum++;
-    if (nextNum < N)
+    var nextNumEnd = nextNum;
+    while (nextNumEnd < input.Length && char.IsNumber(input[nextNumEnd]))
+        nextNumEnd++;
+    if (nextNum < input.Length)
     {
-        var nextNumInt = int.Parse(pair[1]) + (input[nextNum] - '0');
-        input = input.Substring(0, nextNum) + nextNumInt.ToString() + input.Substring(nextNum + 1, N - nextNum - 1);
+        var nextNumString = input.Substring(nextNum, nextNumEnd - nextNum);
+        var nextNumInt = int.Parse(pair[1]) + int.Parse(nextNumString);
+        input = input.Substring(0, nextNum) + nextNumInt.ToString() + input.Substring(nextNumEnd, input.Length - nextNumEnd);
     }
     if (previousNum > 0)
     {
